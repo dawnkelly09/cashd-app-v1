2ef25cc2d4af1114218180c17971bf48bb516183
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-27 03:47:23 -0800

    adds Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Welcome to Cash'd</h1>
+        <h2>Cash'd gives you easy to use tools to find your ideal cannabis strain</h2>
+    </header>
+  )
+}
+
+export default Header
