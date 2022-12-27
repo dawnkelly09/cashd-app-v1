846153cbf0f328cc1d2f03e49622279fdefd86a6
dawnkelly09 <dawnkelly09@gmail.com>
2022-12-27 07:39:27 -0800

    adds onClick to button component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Header from './components/Header'
 import Button from './components/Button'
 import TextArea from './components/TextArea'
-import { useState } from 'react'
+//import generateAction from './generate'
+//import { useState } from 'react'
+
 
 function App() {
-
   
-
+  const onClick = () => {
+    console.log('click')
+  }
   return (
     <div className='container'>
       <Header title="Welcome to Cash'd" />
       <TextArea />
-      <Button color="#0099CC" text="Suggest a Strain"/>
+      <Button color='#0099cc' 
+              text='Suggest a Strain' 
+              onClick={onClick} />
+      
 
       
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,22 @@
+import PropTypes from 'prop-types'
 
+const Button = ({ color, text, onClick }) => {
+  return <button 
+            onClick={onClick}
+            className='btn'
+            style = {{backgroundColor: color}}>
+              {text}
+        </button>
+}
 
-const Button = ({ color, text }) => {
-  return (
-    <button 
-      style={{ backgroundColor: color }} 
-      className='btn'
-      onClick={null}>
-        {text} 
-    </button>
-  )
+Button.defaultProps = {
+  color: '#0099cc',
+}
+
+Button.propTypes = {
+  test: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
